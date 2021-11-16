98266458759f176c63a5d82a104f6e88f4f07ebd
koichi highcom <youhappywith@yahoo.co.jp>
2021-11-16 22:24:24 +0900

    Firestoreから銘柄の一覧を取得してGridViewに表示する

--- lib/sake_grid_view.dart
+++ lib/sake_grid_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:growing_sake/sake_detail.dart';
 
 class SakeGridViewWidget extends StatelessWidget {
   final Color color;
@@ -9,46 +11,61 @@ class SakeGridViewWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    var assetsImage = "images/ic_sake.png";
     return MaterialApp(
       title: 'Grid List',
+      routes: <String, WidgetBuilder> {
+        '/sake_detail': (BuildContext context) => SakeDetailWidget()
+      },
       home: Scaffold(
-        body: GridView.count(
-          padding: const EdgeInsets.all(4.0),
-          crossAxisCount: 3,
-          crossAxisSpacing: 10.0, // 縦
-          mainAxisSpacing: 10.0, // 横
-          childAspectRatio: 0.7, // 高さ
-          shrinkWrap: true,
-          children: List.generate(100, (index) {
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('BrandList').snapshots(),
+          builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
 
-            var assetsImage = "images/ic_sake.png";
-
-            return Container(
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: color,
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(5.0, 5.0),
-                    blurRadius: 10.0,
-                  )
-                ],
-              ),
-              child:Column(
-                  children: <Widget>[
-                    Image.asset(assetsImage, fit: BoxFit.cover,),
-                    Container(
-                      margin: const EdgeInsets.all(16.0),
-                      child: Text(
-                        'Meeage $index',
-                      ),
-                    ),
-                  ]
-              ),
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 10.0, // 縦
+                  mainAxisSpacing: 10.0, // 横
+                  childAspectRatio: 0.7),
+              itemCount: snapshot.data!.docs.length,
+              padding: const EdgeInsets.all(5.0),
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pushNamed('/sake_detail');
+                    },
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset(assetsImage, fit: BoxFit.cover,),
+                        Container(
+                          margin: const EdgeInsets.all(10.0),
+                          child: Text(snapshot.data!.docs[index]['title']),
+                        ),
+                      ],
+                    )),
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: color,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(5.0, 5.0),
+                        blurRadius: 10.0,
+                      )
+                    ],
+                  ),
+                );
+              },
             );
           }),
-        ),
       ),
     );
   }
