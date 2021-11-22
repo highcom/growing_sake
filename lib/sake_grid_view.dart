76e286141259fc51628022251e9bfe18a14b4f08
koichi highcom <youhappywith@yahoo.co.jp>
2021-11-22 19:56:43 +0900

    GridView表示時にAppBarが二重で表示されてしまう修正

--- lib/sake_grid_view.dart
+++ lib/sake_grid_view.dart
@@ -12,62 +12,56 @@ class SakeGridViewWidget extends StatelessWidget {
   Widget build(BuildContext context) {
 
     var assetsImage = "images/ic_sake.png";
-    return MaterialApp(
-      title: 'Grid List',
-      routes: <String, WidgetBuilder> {
-        '/sake_detail': (BuildContext context) => SakeDetailWidget()
-      },
-      home: Scaffold(
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('BrandList').snapshots(),
-          builder: (BuildContext context,
-            AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            return GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 10.0, // 縦
-                  mainAxisSpacing: 10.0, // 横
-                  childAspectRatio: 0.7),
-              itemCount: snapshot.data!.docs.length,
-              padding: const EdgeInsets.all(5.0),
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).pushNamed('/sake_detail');
-                    },
-                    child: Column(
-                      children: <Widget>[
-                        Image.asset(assetsImage, fit: BoxFit.cover,),
-                        Container(
-                          margin: const EdgeInsets.all(10.0),
-                          child: Text(snapshot.data!.docs[index]['title']),
-                        ),
-                      ],
-                    )),
-                  padding: const EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    color: color,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.grey,
-                        offset: Offset(5.0, 5.0),
-                        blurRadius: 10.0,
-                      )
-                    ],
-                  ),
-                );
-              },
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('BrandList').snapshots(),
+        builder: (BuildContext context,
+          AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-          }),
-      ),
+          }
+
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 10.0, // 縦
+                mainAxisSpacing: 10.0, // 横
+                childAspectRatio: 0.7),
+            itemCount: snapshot.data!.docs.length,
+            padding: const EdgeInsets.all(5.0),
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pushNamed('/sake_detail');
+                  },
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset(assetsImage, fit: BoxFit.cover,),
+                      Container(
+                        margin: const EdgeInsets.all(10.0),
+                        child: Text(snapshot.data!.docs[index]['title']),
+                      ),
+                    ],
+                  )),
+                padding: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(5.0, 5.0),
+                      blurRadius: 10.0,
+                    )
+                  ],
+                ),
+              );
+            },
+          );
+        }),
     );
   }
 }
